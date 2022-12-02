956c5ff9b9762ab62b69bc0f034cd0abb31b0ee4
John Firebaugh <john.firebaugh@gmail.com>
2022-12-02 13:38:36 -0800

    Customize example for bug report

--- helloworld-minimal-sample/extension.js
+++ helloworld-minimal-sample/extension.js
@@ -13,17 +13,14 @@ function activate(context) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "helloworld-minimal-sample" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	context.subscriptions.push(vscode.commands.registerCommand('extension.helloWorld', (hello, world) => {
+		vscode.window.showInformationMessage(hello);
+		vscode.window.showInformationMessage(world);
+	}));
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand('extension.executeHelloWorld', () => {
+		vscode.commands.executeCommand('extension.helloWorld', 'hello', 'world')
+	}));
 }
 
 // this method is called when your extension is deactivated
